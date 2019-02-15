5283d975ec411a3a4134fb632ab5637b99838627
Nmancer <datoantadze@gmail.com>
2019-02-15 05:33:10 +0400

    Updates

---
+++ precache-manifest.690b968efa8946b40f810566e2d577c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "915f5d0c579b94b7fae2",
+    "url": "/Giphy-App/static/js/runtime~main.915f5d0c.js"
+  },
+  {
+    "revision": "30d619e8927fafe9b9bc",
+    "url": "/Giphy-App/static/js/main.30d619e8.chunk.js"
+  },
+  {
+    "revision": "9f4c8277efcb84abdcb9",
+    "url": "/Giphy-App/static/js/2.9f4c8277.chunk.js"
+  },
+  {
+    "revision": "30d619e8927fafe9b9bc",
+    "url": "/Giphy-App/static/css/main.e9037637.chunk.css"
+  },
+  {
+    "revision": "5809f3f2eff1a638b0d4af2131d08914",
+    "url": "/Giphy-App/index.html"
+  }
+];
